f8abc056420210099adeb1b2050640a7ae9efe9d
Vincent PETIT <vpetit@e3r4p7.42.fr>
2017-05-11 22:39:16 +0200

    Working on debugging sort fct

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vpetit <vpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 17:03:04 by vpetit            #+#    #+#             */
-/*   Updated: 2017/05/11 19:11:46 by vpetit           ###   ########.fr       */
+/*   Updated: 2017/05/11 20:14:25 by vpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,33 +105,45 @@ t_format_id			*ft_list_prev(t_format_id *elem)
 	prev = elem;
 	if (prev)
 	{
-		prev = prev->first;
-		while (prev->next && prev->next != elem)
-			prev = prev->next;
+		if ((prev = prev->first) != elem)
+		{
+			while (prev->next && prev->next != elem)
+				prev = prev->next;
+		}
 	}
 	return (prev);
 }
 
 void				ft_change_first(t_format_id *elem)
 {
+	int		i;
+
+	i = 0;
 	if (elem)
 	{
 		ft_putstr("--- 1.2.1 ---\n");
 		elem = elem->first;
 
 		elem->first = elem;
+
+		// ft_print_all_format_id(elem);
 		while (elem && elem->next)
 		{
 			ft_putstr("--- 1.2.1... ---\n");
 			elem->next->first = elem->first;
 			elem = elem->next;
+			if (i >= 10)
+				ft_error("Stop");
+			else
+				i++;
 		}
 	}
 }
 
 void				ft_swap_elem(t_format_id *elem1, t_format_id *elem2)
 {
-	t_format_id		*tmp;
+	t_format_id		*tmp1;
+	t_format_id		*tmp2;
 	t_format_id		*prev1;
 	t_format_id		*prev2;
 
@@ -140,19 +152,20 @@ void				ft_swap_elem(t_format_id *elem1, t_format_id *elem2)
 	{
 		prev1 = NULL;
 		prev2 = NULL;
-		tmp = elem1->next;
+		tmp1 = elem1->next;
+		tmp2 = elem2->next;
 		if (elem1 != elem1->first)
 			prev1 = ft_list_prev(elem1);
 		if (elem2 != elem2->first)
 			prev2 = ft_list_prev(elem2);
-		elem1->next = elem2->next;
-		elem2->next = tmp;
+		elem1->next = tmp2;
+		elem2->next = tmp1;
 		((prev1) ? (prev1->next = elem2) : (prev1));
 		((prev2) ? (prev2->next = elem1) : (prev2));
 		if (!prev1)
-			ft_change_first(elem2);
-		if (!prev2)
 			ft_change_first(elem1);
+		if (!prev2)
+			ft_change_first(elem2);
 	}
 }
 
@@ -187,6 +200,8 @@ int					ft_printf(char *str, ...)
 	ft_putstr("PARSING : START\n");
 	format_id = ft_parse_all(str);
 	ft_putstr("PARSING : SUCESS\n");
+
+	ft_print_all_format_id(format_id);
 
 	ft_sort_list(format_id);
 
